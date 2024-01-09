3729099cea62dd3a9309836b243ac927a7be0ef5
LULIU-9 <lu.liu4work@gmail.com>
2024-01-09 11:48:08 +1000

    style: install sass & implement global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import "./app.scss";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <section>hero</section>
+      <section>parallax</section>
+      <section>services</section>
+      <section>parallax</section>
+      <section>project1</section>
+      <section>project2</section>
+      <section>project3</section>
+      <section>contact</section>
+    </div>
+  );
 };
 
 export default App;
