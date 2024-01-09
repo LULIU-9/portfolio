41ccf7b35ecf98b5a51a53fbb4f0b12c2a48a1a5
LULIU-9 <lu.liu4work@gmail.com>
2024-01-09 12:07:53 +1000

    feat:a simple navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return (
     <div>
-      <section>hero</section>
+      <section>
+        <Navbar />
+      </section>
       <section>parallax</section>
       <section>services</section>
       <section>parallax</section>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/*sidebar*/}
+
+      <div className="wrapper">
+        <span>Lu Liu</span>
+        <div className="social">
+          <a href="">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="">
+            <img src="/linkedin.png" alt="" />
+          </a>
+          <a href="">
+            <img src="/youtube.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
