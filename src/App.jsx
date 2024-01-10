47a296e5f61792ac77f88ddfe23d5c401f8e1845
LULIU-9 <lu.liu4work@gmail.com>
2024-01-10 16:17:24 +1000

    feat: implement pallax && services

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
 
 const App = () => {
   return (
@@ -9,9 +11,15 @@ const App = () => {
         <Navbar />
         <Hero />
       </section>
-      <section>parallax</section>
-      <section>services</section>
-      <section>parallax</section>
+      <section id="services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Projects">
+        <Parallax type="Projects" />
+      </section>
       <section>project1</section>
       <section>project2</section>
       <section>project3</section>
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,110 @@
+import { useRef } from "react";
+import "./services.scss";
+import { motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Services = () => {
+  const ref = useRef();
+  const isView = useInView(ref, { margin: "-100px" });
+  return (
+    <motion.div
+      ref={ref}
+      className="services"
+      variants={variants}
+      initial="initial"
+      animate={isView && "animate"}
+      //   whileInView="animate"
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <p>
+          I focus on helping your brand grow
+          <br /> and move forward
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className="titleContainer" variants={variants}>
+        <div className="title">
+          <img src="/people.webp" alt="" />
+          <h1>
+            <motion.b whileHover={{ color: "orange" }}>Unique</motion.b> Idea
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <motion.b whileHover={{ color: "orange" }}>For Your</motion.b>{" "}
+            Business
+          </h1>
+          <button>What we do?</button>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud
+          </p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud
+          </p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud
+          </p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud
+          </p>
+          <button>Go</button>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Services;
