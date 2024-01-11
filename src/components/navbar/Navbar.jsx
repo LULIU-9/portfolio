ef5a92f16a7a7e73bdc9eca2ff20a9a5728e45d9
LULIU-9 <lu.liu4work@gmail.com>
2024-01-11 18:43:19 +1000

    style: favicon && navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,31 +1,35 @@
 import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
 import { motion } from "framer-motion";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaCode } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Sidebar />
+      {/* <Sidebar /> */}
       <div className="wrapper">
-        <motion.span
+        <motion.div
+          className="logo"
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
         >
-          Lu Liu Dev
-        </motion.span>
+          <span>
+            <FaCode size={18} />
+          </span>
+          LU.
+        </motion.div>
         <div className="social">
-          <a href="#">
-            <img src="/facebook.png" alt="" />
+          <a href="https://github.com/LULIU-9">
+            <FaGithub size={20} />
           </a>
-          <a href="#">
-            <img src="/instagram.png" alt="" />
+          <a href="https://www.linkedin.com/in/lu-liu-232983199">
+            <FaLinkedin size={20} />
           </a>
-          <a href="#">
-            <img src="/linkedin.png" alt="" />
-          </a>
-          <a href="#">
-            <img src="/dribbble.png" alt="" />
+          <a href="#Contact">
+            <button>Contact Me</button>
           </a>
         </div>
       </div>
